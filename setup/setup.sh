de32f70d9132e3f212032043218b9029c2222e7b
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-10-21 14:12:30 +0800

    Added GPL text

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#setup.sh - sets up the machine for GlassDrive
+#Copyright (C) 2017  Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation (version 3)
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 echo 'Welcome to GlassDrive!'
 echo 'This script will help you set it up for the first time.'
 
