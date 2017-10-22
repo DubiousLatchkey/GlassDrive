caf994ca512c6fe8663030ca62377550fd6587f7
robin <robin@i2dd.com>
2017-10-22 11:06:10 +0800

    final

---
+++ www/transfer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload: <br>
+    <input type="file" multiple="multiple" name="fileToUpload[]"><br>
+    Override: <input type="checkbox" name="checkBox">
+    <input type="submit" value="Upload Files" name="submit">
+</form>
+
+</body>
+</html>
