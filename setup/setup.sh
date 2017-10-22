b1d6b505c566d6888178488d32264861bbb9392d
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-10-22 10:57:36 +0800

    deps.sh copies config files
    Include conf files in repo
    setup.sh now properly runs sed on rc.local using -E

--- setup/setup.sh
+++ setup/setup.sh
@@ -30,7 +30,7 @@ fi
 GlassDrivePath=$(dirname $(readlink -f $0))
 echo $GlassDrivePath
 cp /etc/rc.local rc.local.old
-sed -e "s/exit.*/$GlassDrivePath\/checknetwork.sh/" /etc/rc.local > rc.local.new
+sed -E -e "s/exit.*/$GlassDrivePath\/checknetwork.sh/" /etc/rc.local > rc.local.new
 mv /etc/rc.local.new /etc/rc.local
 
 . ./adhoc.sh $(./serial.sh)
