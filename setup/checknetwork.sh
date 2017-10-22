ae8caf195d7491f73091ad896d07d48b340f2ff7
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-10-22 11:09:25 +0800

    Fix typo

--- setup/checknetwork.sh
+++ setup/checknetwork.sh
@@ -15,5 +15,5 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-GlassDriveSetupPath=$(dirname $(readline -f $0))
+GlassDriveSetupPath=$(dirname $(readlink -f $0))
 $GlassDriveSetupPath/adhoc.sh $($GlassDriveSetupPath/serial.sh)
